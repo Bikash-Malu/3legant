88b1afcd36d5b535466d95f473a008d1dce72ad8
Bikash Malu <141820540+Bikash-Malu@users.noreply.github.com>
2024-11-25 19:43:17 +0530

    adding

--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -1,19 +1,25 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import tasksReducer from './tasksSlice';
-import authReducer from './authSlice';
-const persistConfig = {
-  key: 'root',
+import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import tasksReducer from "./tasksSlice";
+import authReducer from "./authSlice";
+const tasksPersistConfig = {
+  key: "tasks",
   storage,
-  whitelist: ['auth'],
+};
+
+const authPersistConfig = {
+  key: "auth",
+  storage,
 };
 const rootReducer = {
-  tasks: tasksReducer,
-  auth: persistReducer(persistConfig, authReducer),
+  tasks: persistReducer(tasksPersistConfig, tasksReducer), 
+
+  auth: persistReducer(authPersistConfig, authReducer), 
 };
 
 export const store = configureStore({
   reducer: rootReducer,
 });
+
 export const persistor = persistStore(store);
--- src/components/redux/tasksSlice.js
+++ src/components/redux/tasksSlice.js
@@ -1,6 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { v4 as uuidv4 } from "uuid";
-
 const defaultTasks = [
   { id: 1, text: "Buy groceries", completed: false, important: false },
   { id: 2, text: "Finish project report", completed: false, important: true },
@@ -13,19 +12,10 @@ const defaultTasks = [
   { id: 9, text: "Update blog", completed: true, important: false },
 ];
 
-const loadTasksFromLocalStorage = () => {
-  const savedTasks = localStorage.getItem("tasks");
-  return savedTasks ? JSON.parse(savedTasks) : defaultTasks;
-};
-
-const saveTasksToLocalStorage = (tasks) => {
-  localStorage.setItem("tasks", JSON.stringify(tasks));
-};
-
 const tasksSlice = createSlice({
   name: "tasks",
   initialState: {
-    list: loadTasksFromLocalStorage(),
+    list: defaultTasks,
     loading: false,
   },
   reducers: {
@@ -34,39 +24,33 @@ const tasksSlice = createSlice({
     },
     setTasks: (state, action) => {
       state.list = action.payload;
-      saveTasksToLocalStorage(state.list);
     },
     toggleTaskCompletion: (state, action) => {
       const task = state.list.find((task) => task.id === action.payload);
       if (task) {
         task.completed = !task.completed;
-        saveTasksToLocalStorage(state.list);
       }
     },
     toggleTaskImportance: (state, action) => {
       const task = state.list.find((task) => task.id === action.payload);
       if (task) {
         task.important = !task.important;
-        saveTasksToLocalStorage(state.list);
       }
     },
     deleteTask: (state, action) => {
       state.list = state.list.filter((task) => task.id !== action.payload);
-      saveTasksToLocalStorage(state.list);
     },
     deleteAllTasks: (state) => {
       state.list = [];
-      saveTasksToLocalStorage(state.list);
     },
     addTask: (state, action) => {
       const newTask = {
-        id: uuidv4(), // Generate a unique ID
+        id: uuidv4(), 
         text: action.payload.text,
         completed: false,
         important: false,
       };
       state.list.push(newTask);
-      saveTasksToLocalStorage(state.list);
     },
   },
 });
